48acc171fa55f195a01df078cbeb310e0a886601
ishimweghislain <ishimweghislain82@gmail.com>
2026-02-25 14:46:22 +1300

    donc

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
+const secretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_51PLACEHOLDER';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(secretKey, {
     apiVersion: '2026-01-28.clover' as any,
 });
